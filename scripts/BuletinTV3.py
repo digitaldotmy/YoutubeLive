9bbf9f2dd1454d6792f9d91343a5159a48d80e16
digitaldotmy <83358304+digitaldotmy@users.noreply.github.com>
2023-01-03 16:28:54 +0800

    Update BuletinTV3.py

--- scripts/BuletinTV3.py
+++ scripts/BuletinTV3.py
@@ -13,12 +13,12 @@ def grab(url):
     response = s.get(url, timeout=15).text
     if '.m3u8' not in response:
         if windows:
-            print('https://github.com/digitaldotmy/YoutubeLive/blob/main/assets/Interrupted.m3u8')
+            print('https://raw.githubusercontent.com/digitaldotmy/YoutubeLive/main/assets/Interrupted.m3u')
             return
         os.system(f'wget {url} -O temp.txt')
         response = ''.join(open('temp.txt').readlines())
         if '.m3u8' not in response:
-            print('https://github.com/digitaldotmy/YoutubeLive/blob/main/assets/Interrupted.m3u8')
+            print('https://raw.githubusercontent.com/digitaldotmy/YoutubeLive/main/assets/Interrupted.m3u')
             return
     end = response.find('.m3u8') + 5
     tuner = 100
